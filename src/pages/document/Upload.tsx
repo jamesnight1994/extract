9951e764243b30b32e119d1e4fa5be5a3bacd155
NightravenJames <jamesnight1994@gmail.com>
2024-05-04 00:14:56 +0300

    upload page

--- src/pages/document/Upload.tsx
+++ src/pages/document/Upload.tsx
@@ -1,32 +1,18 @@
-import React, { useState } from "react";
+import React, {  } from "react";
 import {
   Button,
   Flex,
-  Text,
-  TextField,
-  Radio,
-  RadioGroupField,
-  TextAreaField,
+  Input
 } from "@aws-amplify/ui-react";
 
-const initialValues = {
-  name: "",
-  email: "",
-  animal: "Dog",
-  message: "",
-  subscribe: false,
-};
 
 const Upload = () => {
-  const [values, setValues] = useState(initialValues);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
 
-    setValues({
-      ...values,
-      [name]: value,
-    });
+    console.log('Name:', name);
+    console.log('Value:', value)
   };
 
   const handleSubmit = (e) => {
@@ -37,55 +23,11 @@ const Upload = () => {
   return (
     <>
       <Flex as="form" direction="column" width="100%" onSubmit={handleSubmit}>
-        <TextField
-          value={values.name}
+        <Input
           onChange={handleInputChange}
-          name="name"
-          label={
-            <Text>
-              Name
-              <Text as="span" fontSize="0.8rem" color="red">
-                (required)
-              </Text>
-            </Text>
-          }
-          type="text"
-          isRequired={true}
-        />
-        <TextField
-          value={values.email}
-          onChange={handleInputChange}
-          name="email"
-          label={
-            <Text>
-              Email
-              <Text as="span" fontSize="0.8rem" color="red">
-                (required)
-              </Text>
-            </Text>
-          }
-          type="email"
-          isRequired={true}
-        />
+          name="image"
+          id="file-upload" type="file" accept="image/*" />
 
-        <RadioGroupField
-          label="Animal"
-          name="animal"
-          value={values.animal}
-          onChange={handleInputChange}
-        >
-          <Radio value="Dog">Dog</Radio>
-          <Radio value="Cat">Cat</Radio>
-          <Radio value="Bird">Bird</Radio>
-        </RadioGroupField>
-
-        <TextAreaField
-          label="Message"
-          value={values.message}
-          onChange={handleInputChange}
-          name="message"
-          rows={6}
-        />
 
         <Button
           type="submit"
